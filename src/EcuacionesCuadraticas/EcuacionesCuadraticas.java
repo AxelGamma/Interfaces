2614b0da7705198d31d5c7c0556926293ea5f988
Perejiloo <bigottitos098@gmail.com>
2021-03-22 11:15:54 -0600

    Ecuaciones cuadraticas 22-Marzo-2021

--- src/EcuacionesCuadraticas/EcuacionesCuadraticas.java
+++ src/EcuacionesCuadraticas/EcuacionesCuadraticas.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionEvent;
 import java.text.DecimalFormat;
 import javax.swing.*;
 
-public class EcuacionesCuadraticas extends JFrame {
+public class EcuacionesCuadraticas extends JFrame implements ActionListener {
 
     //Variables las etiquetas, los cuadros de texto y el boton
     private JLabel a, b, c;
@@ -26,8 +26,8 @@ public class EcuacionesCuadraticas extends JFrame {
         setResizable(true);//Se bloque los rangos de la ventana
         //Invocando a los metodos
         Componentes();
-        Boton();
         respuestas();
+        Boton();
 
     }
 
@@ -101,74 +101,63 @@ public class EcuacionesCuadraticas extends JFrame {
         aceptar.setText("Hacer Operacion");
         panelCentro.add(aceptar);
         add(panelCentro);
-        //El boton -> ActionOyente se crea un objeto de ActionListener
-        //Agrega un ActionListener al botón
-        //Utilizando el metodo addActionListener  del Componente.
-        //Cuando ocurre el even de accion, se invoca el metodo actionPerformed de ese objeto
-        //Clase anonima
 
-
-        aceptar.addActionListener(new ActionListener() {
-            @Override
-            //Este metodo envia un evento
-            public void actionPerformed(ActionEvent e) {
-
-                //Estamos enviando el evento al metodo aceptarActionPerformed
-                aceptarActionPerformed(e);
-            }
-
-            private void aceptarActionPerformed(ActionEvent e) {
-
-                DecimalFormat formato = new DecimalFormat("####.###");
-
-                try {
-
-                    double a1, b1, c1, primerR, segundaR, denominador, primeraX = 0, segundaX = 0;
-
-                    a1 = Double.parseDouble(at.getText());
-                    b1 = Double.parseDouble(bt.getText());
-                    c1 = Double.parseDouble(ct.getText());
-
-                    //Sacando la raiz de 4ac
-                    primerR = (4 * a1 * c1);
-                    //Sacando la b elevado al cuadrado
-                    segundaR = ((b1 * b1) - (primerR));
-                    //Denominador
-                    denominador = ((2) * (a1));
-
-                    //Si es mayor a cero
-                    if (segundaR > 0) {
-                        //primeraX guarda el resulatado
-                        primeraX = ((-1 * b1) + Math.sqrt(segundaR)) / denominador;
-                        //A primeraX se le da el formato que queremos, luego valueOf lo convierte a String y lo guarda en la variable
-                        //Cadena
-                        String cadena1 = String.valueOf(formato.format(primeraX));
-                        x1.setText(cadena1);
-
-                        segundaX = ((-1 * b1) - Math.sqrt(segundaR)) / denominador;
-                        String cadena2 = String.valueOf(formato.format(segundaX));
-                        x2.setText(cadena2);
-
-                    }
-                    //Numero imaginario, no tiene raiz en los numeros reales
-                    else{
-                        x1.setText((-1*b1)+"+"+"√"+segundaR+"i"+"/"+(2*a1)); // si es un numero imaginario
-                        x2.setText((-1*b1)+"-"+"√"+segundaR+"i"+"/"+(2*a1));
-                    }
-
-                } catch (NumberFormatException evento) {
-                    x1.setText("Error");
-                    x2.setText("Error");
-                }
-            }
-
-        });
-
+        aceptar.addActionListener(this);
 
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        aceptar(e);
+    }
+
+        private void aceptar(ActionEvent evt){
+            DecimalFormat formato = new DecimalFormat("####.###");
+
+            try {
+
+                double a1, b1, c1, primerR, segundaR, denominador, primeraX = 0, segundaX = 0;
+
+                a1 = Double.parseDouble(at.getText());
+                b1 = Double.parseDouble(bt.getText());
+                c1 = Double.parseDouble(ct.getText());
+
+                //Sacando la raiz de 4ac
+                primerR = (4 * a1 * c1);
+                //Sacando la b elevado al cuadrado
+                segundaR = ((b1 * b1) - (primerR));
+                //Denominador
+                denominador = ((2) * (a1));
+
+                //Si es mayor a cero
+                if (segundaR > 0) {
+                    //primeraX guarda el resulatado
+                    primeraX = ((-1 * b1) + Math.sqrt(segundaR)) / denominador;
+                    //A primeraX se le da el formato que queremos, luego valueOf lo convierte a String y lo guarda en la variable
+                    //Cadena
+                    String cadena1 = String.valueOf(formato.format(primeraX));
+                    x1.setText(cadena1);
+
+                    segundaX = ((-1 * b1) - Math.sqrt(segundaR)) / denominador;
+                    String cadena2 = String.valueOf(formato.format(segundaX));
+                    x2.setText(cadena2);
+
+                }
+                //Numero imaginario, no tiene raiz en los numeros reales
+                else{
+                    x1.setText((-1*b1)+"+"+"√"+segundaR+"i"+"/"+(2*a1)); // si es un numero imaginario
+                    x2.setText((-1*b1)+"-"+"√"+segundaR+"i"+"/"+(2*a1));
+                }
+
+            } catch (NumberFormatException evento) {
+                x1.setText("Error");
+                x2.setText("Error");
+            }
+
+        }
 
 }
+
 
 
 
